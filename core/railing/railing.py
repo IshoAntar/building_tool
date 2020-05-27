359e6b0663fb12e7f5d9c8c0a3c8b82566110388
Isho Antar <isho.antar1@gmail.com>
2020-05-27 09:16:46 +0300

    Fix the crash when adjusting the railing offset.

--- core/railing/railing.py
+++ core/railing/railing.py
@@ -39,7 +39,10 @@ def make_fill(bm, face, prop):
     dup_face = filter_geom(ret["geom"], BMFace)[0]
     vertical = filter_vertical_edges(dup_face.edges, dup_face.normal)
     non_vertical = [e for e in dup_face.edges if e not in vertical]
-    top_edge = sort_edges(non_vertical, Vector((0., 0., -1.)))[0]
+    sorted_edges = sort_edges(non_vertical, Vector((0., 0., -1.)))
+    if not sorted_edges:
+        return
+    top_edge = sorted_edges[0]
     bmesh.ops.translate(bm, verts=top_edge.verts, vec=Vector((0., 0., -1.))*prop.corner_post_width/2)
 
     # create railing top
